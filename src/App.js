09509b12932a78a625ef29658832acc885263813
ndb77 <bastolanoah@gmail.com>
2024-10-27 14:46:07 -0500

    more commits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import InfoSection from './components/InfoSection';
